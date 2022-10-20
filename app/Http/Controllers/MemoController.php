82baf7531a47d2c13c23f927806c177ecc49373f
HAYATO SUGAWARA <hy.sugawara03@gmail.com>
2022-10-20 18:00:15 +0900

    一覧機能作成

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -7,6 +7,16 @@ use Illuminate\Http\Request;
 
 class MemoController extends Controller
 {
+    // indexページへ移動
+    public function index()
+    {
+        // モデル名::テーブル全件取得
+        $memos = Memo::all();
+        // memosティレクトリーの中のindexページを指定し、memosの連想配列を代入
+        return view('memos.index', ['memos' => $memos]);
+    }
+
+    // showページへ移動
     public function show($id)
     {
         $memo = Memo::find($id);
--- resources/views/memos/index.blade.php
+++ resources/views/memos/index.blade.php
@@ -11,9 +11,12 @@
 </head>
 
 <body>
-    <a href="/memos">戻る</a>
-    <h1>{{ $memo->title }}</h1>
-    <p>{!! nl2br(e($memo->body)) !!}</p>
+    <h1>タイトル一覧</h1>
+    <ul>
+        @foreach($memos as $memo)
+        <li><a href='/memos/{{ $memo->id }}'>{{ $memo->title }}</a></li>
+        @endforeach
+    </ul>
 </body>
 
 </html>
